2aa6b295cf1eb7e985663b47f39f8023724002d1
mehmetcesur <mehmetceesur@gmail.com>
2023-11-11 18:49:29 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DegerVeReferansTipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ==30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 300, 400, 500 };
+
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+
+            //sayilar1[0] 999
+
+        }
+    }
+}
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi : " + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi) //bunu kullanmak kullanışlı değil class kullanmamız lazım
+        {
+            Console.WriteLine("Sepete eklendi : " + urunAdi);
+
+        }
+
+    }
+}
